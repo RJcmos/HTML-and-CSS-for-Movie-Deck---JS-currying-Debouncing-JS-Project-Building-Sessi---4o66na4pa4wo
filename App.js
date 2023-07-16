8822c2e283d01195ba3a463bd0af72f5116476b0
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-07-16 21:05:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,173 +1,85 @@
-// Fetch movies from the external API
-async function fetchMovies(){
-	try{
-		const options = {
-		  method: 'GET',
-		  headers: {
-			accept: 'application/json',
-			Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmI3MDRmZDY5MTEwYjRhNDgxYzE0ODJiZDViNDE3YyIsInN1YiI6IjY0OTFkNmViYzJmZjNkMDBlMmUyMzJlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RmaUsxDrB9p__eh7pAA5hPQ2qcTmdIck2L_jFtWuNUs'
-		  }
-		};
-		const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`
 
-			const response = await fetch(url, options)
+// API Key - Replace 'YOUR_API_KEY' with your actual API key
+const apiKey = '2ULLRZ37RYB2LP7M';
 
-			const data = await response.json();
-			const movies = data.results;
-			displayMovies(movies);
-
-			console.log(data, "data debug")
-
-			} catch(error) {
-
-			console.log('Error fetching movies:', error);
-
-		}
-}
-
-// Display movies on the web page
-function displayMovies(movies) {
-  const movieList = document.querySelector('.movie-list ul');
-  movieList.innerHTML = '';
-
-  movies.forEach(movie => {
-    const imageUrl = 'https://image.tmdb.org/t/p/original/' + movie.poster_path;
-    const title = movie.title || 'movie-title';
-    const voteCount = movie.vote_count || 'vote-count';
-    const voteAverage = movie.vote_average || 'vote-average';
-
-    const movieCard = document.createElement('div');
-    movieCard.classList.add('movie-card');
-    movieCard.innerHTML = `
-      <img src="${imageUrl}" alt="Movie Poster">
-      <h2>${title}</h2>
-      <p class="vote-count">${voteCount} votes</p>
-      <p class="vote-average">${voteAverage} rating</p>
-      <i class="far fa-heart"></i>
-    `;
-
-    movieList.appendChild(movieCard);
-  });
-}
-
-// Sort movies by release date (oldest to latest)
-function sortByDateOldestToLatest() {
-  const movies = Array.from(document.querySelectorAll('.movie-list .movie-card'));
-  movies.sort((a, b) => {
-    const aDate = new Date(a.dataset.releaseDate);
-    const bDate = new Date(b.dataset.releaseDate);
-    return aDate - bDate;
-  });
-  movies.forEach(movie => {
-    movie.parentNode.appendChild(movie);
-  });
-}
-
-// Sort movies by release date (latest to oldest)
-function sortByDateLatestToOldest() {
-  const movies = Array.from(document.querySelectorAll('.movie-list .movie-card'));
-  movies.sort((a, b) => {
-    const aDate = new Date(a.dataset.releaseDate);
-    const bDate = new Date(b.dataset.releaseDate);
-    return bDate - aDate;
-  });
-  movies.forEach(movie => {
-    movie.parentNode.appendChild(movie);
-  });
-}
-
-// Sort movies by rating (least to most)
-function sortByRatingLeastToMost() {
-  const movies = Array.from(document.querySelectorAll('.movie-list .movie-card'));
-  movies.sort((a, b) => {
-    const aRating = parseFloat(a.dataset.voteAverage);
-    const bRating = parseFloat(b.dataset.voteAverage);
-    return aRating - bRating;
-  });
-  movies.forEach(movie => {
-    movie.parentNode.appendChild(movie);
-  });
-}
-
-// Sort movies by rating (most to least)
-function sortByRatingMostToLeast() {
-  const movies = Array.from(document.querySelectorAll('.movie-list .movie-card'));
-  movies.sort((a, b) => {
-    const aRating = parseFloat(a.dataset.voteAverage);
-    const bRating = parseFloat(b.dataset.voteAverage);
-    return bRating - aRating;
-  });
-  movies.forEach(movie => {
-    movie.parentNode.appendChild(movie);
-  });
-}
-
-// Search movies by name
-function searchMovies() {
-  const searchTerm = document.querySelector('.search-bar input').value.toLowerCase();
-  const movies = Array.from(document.querySelectorAll('.movie-list .movie-card'));
-  
-  movies.forEach(movie => {
-    const title = movie.querySelector('h2').textContent.toLowerCase();
-    if (title.includes(searchTerm)) {
-      movie.style.display = 'block';
-    } else {
-      movie.style.display = 'none';
-    }
-  });
-}
-
-// Toggle favorite status of a movie
-function toggleFavorite() {
-  this.classList.toggle('fas');
-  this.classList.toggle('far');
-
-  const movieTitle = this.parentNode.querySelector('h2').textContent;
-  const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
-
-  if (this.classList.contains('fas')) {
-    // Add movie to favorites
-    if (!favoriteMovies.includes(movieTitle)) {
-      favoriteMovies.push(movieTitle);
-    }
-  } else {
-    // Remove movie from favorites
-    const index = favoriteMovies.indexOf(movieTitle);
-    if (index > -1) {
-      favoriteMovies.splice(index, 1);
-    }
-  }
-
-  localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
-}
-
-// Load favorite movies from local storage
-function loadFavoriteMovies() {
-  const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
-  const movies = document.querySelectorAll('.movie-list .movie-card');
-
-  movies.forEach(movie => {
-    const movieTitle = movie.querySelector('h2').textContent;
-    const favoriteIcon = movie.querySelector('i');
+// Function to fetch stock data from the API
+async function fetchStockData(symbol, timeframe) {
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_${timeframe}&symbol=${symbol}&apikey=${apiKey}`;
     
-    if (favoriteMovies.includes(movieTitle)) {
-      favoriteIcon.classList.add('fas');
-      favoriteIcon.classList.remove('far');
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching stock data:', error);
     }
-  });
 }
 
-// Initialize the web page
-function init() {
-  fetchMovies();
-  loadFavoriteMovies();
-
-  document.querySelector('.sorting-options button:nth-child(1)').addEventListener('click', sortByDateOldestToLatest);
-  document.querySelector('.sorting-options button:nth-child(2)').addEventListener('click', sortByRatingLeastToMost);
-  document.querySelector('.sorting-options button:nth-child(3)').addEventListener('click', sortByDateLatestToOldest);
-  document.querySelector('.sorting-options button:nth-child(4)').addEventListener('click', sortByRatingMostToLeast);
-  document.querySelector('.search-bar button').addEventListener('click', searchMovies);
-  document.querySelectorAll('.movie-list .movie-card i').forEach(icon => icon.addEventListener('click', toggleFavorite));
+// Function to add a stock to the watchlist
+function addStockToWatchlist(symbol, timeframe) {
+    const watchlist = document.getElementById('watchlist');
+    
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.textContent = symbol;
+    card.addEventListener('click', () => openModal(symbol, timeframe));
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        card.remove();
+    });
+    
+    card.appendChild(deleteButton);
+    watchlist.appendChild(card);
 }
 
-init();
+// Function to open the modal and display stock data
+async function openModal(symbol, timeframe) {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
+    
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+    
+    const closeContainer = document.createElement('div');
+    closeContainer.classList.add('modal-close');
+    
+    const closeButton = document.createElement('button');
+    closeButton.textContent = 'Close';
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+        modalContent.remove();
+    });
+    
+    closeContainer.appendChild(closeButton);
+    modalContent.appendChild(closeContainer);
+    
+    const stockData = await fetchStockData(symbol, timeframe);
+    // Process and display the stock data in the modal
+    
+    modal.appendChild(modalContent);
+}
+
+// Event listener for the add button
+const addButton = document.getElementById('add-button');
+addButton.addEventListener('click', () => {
+    const symbolInput = document.getElementById('symbol-input');
+    const symbol = symbolInput.value.toUpperCase();
+    
+    const selectedTimeframeButton = document.querySelector('.timeframe-button.selected');
+    const timeframe = selectedTimeframeButton.dataset.timeframe;
+    
+    addStockToWatchlist(symbol, timeframe);
+    symbolInput.value = '';
+});
+
+// Event listener for the timeframe buttons
+const timeframeButtons = document.querySelectorAll('.timeframe-button');
+timeframeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        timeframeButtons.forEach((btn) => btn.classList.remove('selected'));
+        button.classList.add('selected');
+    });
+});
